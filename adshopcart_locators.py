dde54fc32c369bbf73b6035f2a2cafe6a1f930fa
Zoya <zoya@clearviewsys.com>
2022-03-26 09:17:14 -0700

    methods signUp, check_full_name and check_orders added

--- adshopcart_locators.py
+++ adshopcart_locators.py
@@ -1,4 +1,27 @@
+
+from faker import Faker
+fake = Faker(locale=['en_CA', 'en_US'])
+
 app = "Advantage Online Shopping"
 aos_URL = "https://advantageonlineshopping.com/#/"
-aos_homepage_titel = f"\u00A0Advantage Shopping"
+#aos_homepage_title = f"\u00A0Advantage Shopping"
 #aos_homepage_title = "&nbspAdvantage Shopping".replace('&nbsp', '')
+aos_homepage_title = " Advantage Shopping"
+aos_register_URL = "https://advantageonlineshopping.com/#/register"
+new_username = fake.user_name()
+new_password = fake.password()
+email = fake.email()
+first_name = fake.first_name()
+last_name = fake.last_name()
+phonenum = fake.phone_number()
+full_name = f'{first_name} {last_name}'
+address = fake.street_address()
+
+while len(fake.city())>9 or len(fake.city())==0 :
+    city = fake.city()
+
+short_city = city
+
+country = fake.current_country()
+state = fake.state()
+zip_code = fake.zipcode()
--- adshopcart_methods.py
+++ adshopcart_methods.py
@@ -1,12 +1,17 @@
 import datetime
+
+import selenium
+from selenium.webdriver.support.ui import Select  # <--- add this import for drop down lists
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import adshopcart_locators as locators
 
 
 s = Service(executable_path='../chromedriver.exe')
 driver = webdriver.Chrome(service=s)
+
 
 def setUp():
     print(f'Test started at: {datetime.datetime.now()}')
@@ -27,6 +32,84 @@ def setUp():
         #tearDown()
 
 
+def sign_up():
+    driver.find_element(By.ID, "menuUser").click()
+    sleep(1)
+    print("The sign-in window pops up")
+    if driver.find_element(By.XPATH, '//div[contains(@class, "login ng-scope")]').is_displayed():
+        print("we r on pop window")
+    #driver.find_element(By.XPATH, '//div[contains(@class, "login ng-scope")]').click()
+    sleep(1)
+    if driver.find_element(By.XPATH, '//a[contains(., "CREATE NEW ACCOUNT")]').is_displayed():
+        print("found the Create New Account")
+    driver.find_element(By.XPATH, '//a[contains(., "CREATE NEW ACCOUNT")]').click()
+    print("navigating to Create New Account page")
+
+    print(driver.current_url)
+
+    print("Starting with new user registration")
+
+    driver.find_element(By.XPATH, "//input[@name='usernameRegisterPage']").send_keys(locators.new_username)
+    driver.find_element(By.XPATH, "//input[@name='emailRegisterPage']").send_keys(locators.email)
+    driver.find_element(By.XPATH, "//input[@name='passwordRegisterPage']").send_keys(locators.new_password)
+    driver.find_element(By.XPATH, "//input[@name='confirm_passwordRegisterPage']").send_keys(locators.new_password)
+    driver.find_element(By.XPATH, "//input[@name='first_nameRegisterPage']").send_keys(locators.first_name)
+
+    driver.find_element(By.XPATH, "//input[@name='last_nameRegisterPage']").send_keys(locators.last_name)
+
+    driver.find_element(By.XPATH, "//input[@name='phone_numberRegisterPage']").send_keys(locators.phonenum)
+
+    Select(driver.find_element(By.TAG_NAME, "select")).select_by_visible_text("Canada")
+    sleep(1)
+    driver.find_element(By.XPATH, "//input[@name='cityRegisterPage']").send_keys(locators.short_city)
+    sleep(1)
+    driver.find_element(By.XPATH, "//input[@name='addressRegisterPage']").send_keys(locators.address)
+
+    driver.find_element(By.XPATH, "//input[@name='state_/_province_/_regionRegisterPage']").send_keys(locators.state)
+
+    driver.find_element(By.XPATH, "//input[@name='postal_codeRegisterPage']").send_keys(locators.zip_code)
+    driver.find_element(By.XPATH, "//input[@name='i_agree']").click()
+    driver.find_element(By.ID, "register_btnundefined").click()
+    sleep(0.5)
+    assert driver.find_element(By.LINK_TEXT, f"{locators.new_username}")
+
+def check_full_name():
+    driver.find_element(By.ID, "menuUserLink").click()
+    assert driver.find_element(By.ID, "loginMiniTitle").is_displayed()
+    #Select(driver.find_element(By.ID, "loginMiniTitle")).select_by_value('My account')
+    driver.find_element(By.XPATH, "(//label[text()='My account'])[2]").click()
+    #assert driver.find_element(By.LINK_TEXT, "Account details").is_displayed()
+    #fullname = (selenium.getText('//label[@class="ng-binding"][1]')).trim()
+
+
+    if driver.find_element(By.XPATH, "//div[@class='borderBox']//label"):
+        print ("full name box is found")
+
+        fullname = driver.find_element(By.XPATH, "//div[@class='borderBox']//label").get_attribute('innerText')
+        print (fullname)
+
+        if locators.full_name == fullname:
+            print("Full name is found in My Account")
+
+        #if driver.find_element(By.XPATH, f"//label[normalize - space(text()) = '{locators.full_name}']"):
+
+
+    # if driver.find_element(By.XPATH, "//label[text()[normalize-space()='Zoya Salehi']]"):
+    #     print("Full name is found in My Account")
+    #driver.find_element(By.LINK_TEXT, f"{fullname}" )
+
+
+def check_orders():
+    driver.find_element(By.ID, "menuUserLink").click()
+    assert driver.find_element(By.ID, "loginMiniTitle").is_displayed()
+    driver.find_element(By.XPATH, "(// label[text() = 'My orders'])[2]").click()
+
+    assert driver.find_element(By.XPATH, "//label[text()=' - No orders - ']")
+
+    print("User has no ordrs yet")
+
+
+
 def tearDown():
     if driver is not None:
         print(f'-------------------------~*~--------------------------')
@@ -35,5 +118,8 @@ def tearDown():
         driver.close()
         driver.quit()
 
-# setUp()
-# tearDown()
+setUp()
+sign_up()
+check_full_name()
+check_orders()
+tearDown()
